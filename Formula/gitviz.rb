7f6a23143ab37bcf1b9a26f964f09a196359fc61
Fuji <fujiji.entire1414@gmail.com>
2025-06-28 22:10:37 +0900

    Update gitviz to v0.1.1 with -graph flag

--- Formula/gitviz.rb
+++ Formula/gitviz.rb
@@ -1,7 +1,7 @@
 class Gitviz < Formula
     desc "Visualize your local Git contributions like GitHub"
     homepage "https://github.com/anton-fuji/gitviz"
-    url "https://github.com/anton-fuji/gitviz/archive/refs/tags/v0.1.0.tar.gz" 
+    url "https://github.com/anton-fuji/gitviz/archive/refs/tags/v0.1.1.tar.gz" 
     sha256 "a28b8a0a39871551ad835cb69968fe6634ef83d0e0b153730e92534cfeb41162"
   
     license "MIT"
