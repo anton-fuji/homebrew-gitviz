3b87cac8a399ec8c69f2459960ff49c0da4c0599
Fuji <fujiji.entire1414@gmail.com>
2025-06-29 12:09:36 +0900

    v0.1.2

--- Formula/gitviz.rb
+++ Formula/gitviz.rb
@@ -1,8 +1,8 @@
 class Gitviz < Formula
     desc "Visualize your local Git contributions like GitHub"
     homepage "https://github.com/anton-fuji/gitviz"
-    url "https://github.com/anton-fuji/gitviz/archive/refs/tags/v0.1.1.tar.gz" 
-    sha256 "a28b8a0a39871551ad835cb69968fe6634ef83d0e0b153730e92534cfeb41162"
+    url "https://github.com/anton-fuji/gitviz/archive/refs/tags/v0.1.2.tar.gz" 
+    sha256 "4f74c8fb5d6b2af7d4fc8086de02d558cc066f67fb722be1b23d71fdf0dc2c5b"
   
     license "MIT"
   
